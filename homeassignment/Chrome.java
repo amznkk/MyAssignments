55679531b2eb3dc09c41888ae33b24649173edbf
amznkk <kanagarajkarthika24@gmail.com>
2024-10-15 22:47:17 -0700

    Add files via upload
    Week1HomeAssignment

---
+++ homeassignment/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1.homeassignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(false);
+		System.out.println(2008);
+		System.out.println(shortcutKey);
+
+	}
+
+}
---
+++ homeassignment/LibraryManagement.java
@@ -0,0 +1,12 @@
+package week1.day1.homeassignment;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Library libs = new Library ();
+libs.addBook("Harry Potter");
+libs.issueBook();
+	}
+
+}
